5e61015ae10338ad94ebd7a87525a9d0eac41069
MD. SAZZAD <mdsazzad0002@gmail.com>
2024-04-29 09:45:39 +0600

    Update config.php
    update order status by invoice id

--- config.php
+++ config.php
@@ -132,29 +132,29 @@
 
         public function status_by_invoice($invoice){
             // need $invoice id
-            
-            $curl = curl_init();
 
-            curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->base_url.'status_by_invoice/'.$invoice,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-                'Api-Key:'. $this->Api_Key,
-                'Content-Type: application/json',
-                'Secret-Key:'. $this->Secret_Key
-            ),
-            ));
+                $curl = curl_init();
 
-            $response = curl_exec($curl);
+                curl_setopt_array($curl, array(
+                CURLOPT_URL =>  $this->base_url.'status_by_invoice/'.$invoice,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json',
+                    'Secret-Key:'. $this->Secret_Key,
+                    'Api-Key: '.$this->Api_Key,
+                ),
+                ));
 
-            curl_close($curl);
-            return json_decode($response);
+                $response = curl_exec($curl);
+
+                curl_close($curl);
+                return json_decode($response);
 
         }
 
